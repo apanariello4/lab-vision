73ff51f11fb01c378e94b2309003c15da378dd43
apanariello <killetore@gmail.com>
2020-03-16 15:43:23 +0100

    fix

--- lab_01c.py
+++ lab_01c.py
@@ -15,10 +15,10 @@ kernel = np.random.rand(oC, iC, kH, kW)
 n, iC, H, W = input.shape
 oC, _, kH, kW = kernel.shape
 out = np.zeros((n,oC,H-(kH-1),W-(kW-1)))
-#for n_img in range(n):
+
 for c in range(oC):
     for h in range(H-(kH-1)):
         for w in range(W-(kW-1)):
-            out[:,:,h,w] = np.sum((input[:,:,h:h+kH,w:w+kW]*kernel[c,:,:,:,]))
+            out[:,:,h,w] = np.sum((input[:,:,h:h+kH,w:w+kW]*kernel[c,:,:,:]))
 
 print(out)
