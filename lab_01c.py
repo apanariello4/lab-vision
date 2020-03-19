317dbefe0238cc9783214232fa92bdb4a7f615a0
apanariello <killetore@gmail.com>
2020-03-19 10:54:36 +0100

    Added 2 for solutions

--- lab_01c.py
+++ lab_01c.py
@@ -12,13 +12,33 @@ kW = random.randint(2, 6)
 input = np.random.rand(n, iC, H, W)
 kernel = np.random.rand(oC, iC, kH, kW)
 
-n, iC, H, W = input.shape
-oC, _, kH, kW = kernel.shape
+#n, iC, H, W = input.shape
+#oC, _, kH, kW = kernel.shape
 out = np.zeros((n,oC,H-(kH-1),W-(kW-1)))
 
-for c in range(oC):
+def conv3for(input,kernel):#non corretto
+    for c in range(oC):
+        for h in range(H-(kH-1)):
+            for w in range(W-(kW-1)):
+                out[:,:,h,w] = np.sum((input[:,:,h:h+kH,w:w+kW]*kernel[c,:,:,:]))
+    return out
+
+
+def conv4for(input,kernel):#sicuramente corretto
+    out = np.zeros((n,oC,H-(kH-1),W-(kW-1)))
+    for n_img in range(n):
+        for c in range(oC):
+            for h in range(H-(kH-1)):
+                for w in range(W-(kW-1)):
+                    out[n_img,c,h,w] = np.sum((input[n_img,:,h:h+kH,w:w+kW]*kernel[c,:,:,:]))
+    return out
+
+def baraldiconv(input,kernel):
     for h in range(H-(kH-1)):
         for w in range(W-(kW-1)):
-            out[:,:,h,w] = np.sum((input[:,:,h:h+kH,w:w+kW]*kernel[c,:,:,:]))
+            this_input = np.expand_dims(input[:,:,h:h+kH,w:w+kW], 1)
+            this_kernel = np.expand_dims(kernel, 0)
+            out[:,:,h,w] = np.sum(this_input * this_kernel, axis=(-1,-2,-3))
+    return out
 
-print(out)
+print(np.array_equal(baraldiconv(input,kernel),conv4for(input,kernel)))
