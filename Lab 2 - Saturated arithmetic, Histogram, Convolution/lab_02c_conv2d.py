966878c739f86b4622f9ca08176fbfec3ff16d46
apanariello <killetore@gmail.com>
2020-03-20 15:42:17 +0100

    Names

--- Lab 2 - Saturated arithmetic, Histogram, Convolution/lab_02c_conv2d.py
+++ Lab 2 - Saturated arithmetic, Histogram, Convolution/lab_02c_conv2d.py
@@ -16,15 +16,7 @@ kernel = np.random.rand(oC, iC, kH, kW)
 #oC, _, kH, kW = kernel.shape
 out = np.zeros((n,oC,H-(kH-1),W-(kW-1)))
 
-def conv3for(input,kernel):#non corretto
-    for c in range(oC):
-        for h in range(H-(kH-1)):
-            for w in range(W-(kW-1)):
-                out[:,:,h,w] = np.sum((input[:,:,h:h+kH,w:w+kW]*kernel[c,:,:,:]))
-    return out
-
-
-def conv4for(input,kernel):#sicuramente corretto
+def conv4for(input,kernel):
     out = np.zeros((n,oC,H-(kH-1),W-(kW-1)))
     for n_img in range(n):
         for c in range(oC):
